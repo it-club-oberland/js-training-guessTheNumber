8b73321966c2798ffae76c886906cbea8c8c3fe4
Mehmet Dogan <info@intelligentworks.ch>
2017-09-06 07:26:30 +0200

    create inital logic

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+/**
+ * Please provide all your functions in this file.
+ */
+
+
+function generateANumber(pMax){
+    return 0;
+}
---
+++ js/game.js
@@ -0,0 +1,13 @@
+let currentStep = 1;
+let maxNumberOfStep = 5
+let maxOfGeneratedNumber = 100;
+let generatedNumer = null;
+
+
+generatedNumer = generateANumber(maxOfGeneratedNumber);
+
+// Game loop
+while(currentStep > maxNumberOfStep){
+    let userGuess = getUserGuess("Bitte geben Sie Ihre Zahl!");
+    
+}
