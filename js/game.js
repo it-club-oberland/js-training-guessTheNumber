b26d49f8f14a55a461c89b48f0fec05d4bc08e8f
cemildo <cemil82@gmail.com>
2017-09-12 00:07:05 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -5,28 +5,39 @@
 "use strict";
 
 resetGame();
-generatedNumber = generateANumber(maxOfGeneratedNumber);
-
 // Game loop
 while(currentStep < maxNumberOfStep){
     let userGuess = getUserGuess("Bitte geben Sie Ihre Zahl!", maxOfGeneratedNumber);
-    
     if(userGuess == generatedNumber){
         feedback("Yeay! Sie haben das Spiel gewonnen!");
-        resetGame();
-        break;
-    }else{
+        if(confirm("Do you want to play again")){
+          resetGame();
+        } else {
+          break;
+        }
+    } else {
         // Eger kullanicinin girdigi sayi bilgisayarin tuttugundan kücük ise 
         // "Lütfen sayiyi büyütünüz" Feedback ini verecek
         // Tersi durumda da "Lütfen sayiyi kücültünüz" gösterecek
-        feedback("yaklastin");
+        // feedback("yaklastin");
+        if(userGuess < generatedNumber) {
+            feedback("Too Low");
+        } else {
+            feedback("Too High");
+        }
+        if(confirm("do you give up")){
+            resetGame();
+            break;
+        }
     }
-    let a= 5;
-    a++
+
     currentStep++; // currentStep = currentStep + 1;
 }
 
 
 
+if(currentStep === 5){
+    feedback("Oooh! Leider haben Sie das Spiel verloren!");
+    resetGame();
+}
 
-feedback("Oooh! Leider haben Sie das Spiel verloren!");
